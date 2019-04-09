82243a8333c668c5f2e98dee0edf07af2910be2f
emzoo16 <emilyzou98@gmail.com>
2019-04-09 12:21:56 +1200

    resolved conflicts

--- src/Character/Player.java
+++ src/Character/Player.java
@@ -1,7 +1,18 @@
+
 package Character;
+
 
 public class Player extends Character {
 
     private int lives;
+
+
+    public int getLives(){
+        return lives;
+    }
+
+    public void loseLife(){
+        lives--;
+    }
 
 }
--- src/Level.java
+++ src/Level.java
@@ -1,16 +1,32 @@
+
 import Character.Enemy;
 import Character.Player;
+import Item.Puzzle;
 
 
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 /*
-Class that handles the co-ordination between the player, the enemies on the screen and the platforms on the screen.
+Class that handles the co-ordination between the player, the enemies on the screen and the platforms on the screen. Handles
+the logistics of the level like the current score, which enemies are killed and the number of lives of the player.
  */
 public class Level {
 
-    private Player player;
-    private ArrayList<Enemy> Enemy;
+    BufferedImage background;
 
+
+
+    private Player player;
+    private ArrayList<Enemy> enemies;
+    private ArrayList<Characters.Platform> platforms;
+    private Puzzle puzzle;
+
+    /*
+    Method for painting the current level i.e backgrounds,players, enemies and platforms.
+     */
+    public void paintLevel(){
+
+    }
 
 }
