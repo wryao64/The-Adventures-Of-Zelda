7aa7fb9d76a8134135afc647d3cf2b000440a946
Wendy <wryao64@gmail.com>
2019-04-10 16:03:32 +1200

    basic welcome screen with label and button

--- src/View/WelcomeScreen.java
+++ src/View/WelcomeScreen.java
@@ -1,4 +1,25 @@
 package View;
 
-public class WelcomeScreen {
+import javax.swing.*;
+import java.awt.*;
+
+public class WelcomeScreen extends JPanel {
+    private final int BORDER_SIZE = 30;
+
+    public WelcomeScreen() {
+//        JPanel displayPane = new JPanel();
+        this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.setBackground(Color.red); // TESTING
+
+        Label welcomeLabel = new Label("The Legend of Zelda");
+        welcomeLabel.setBackground(Color.blue);
+        welcomeLabel.setAlignment(JLabel.CENTER); //TODO: DOES THIS WORK??
+
+        Button playButton = new Button("PLAY");
+
+        this.add(welcomeLabel);
+        this.add(playButton);
+        this.setBorder(BorderFactory.createEmptyBorder(BORDER_SIZE, BORDER_SIZE, BORDER_SIZE, BORDER_SIZE));
+    }
 }
