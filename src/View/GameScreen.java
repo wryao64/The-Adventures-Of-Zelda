fb0a57ec64c89181b9ef3ea6c53ff56cfa7b37f0
Wendy <wryao64@gmail.com>
2019-04-16 21:38:17 +1200

    slightly refactored pause screen

--- src/View/GameScreen.java
+++ src/View/GameScreen.java
@@ -12,15 +12,14 @@ import java.awt.event.KeyListener;
 
 public class GameScreen extends JPanel implements KeyListener {
     GameController gameController;
+    PauseScreen pauseScreen = new PauseScreen();
+
+    Color glassPaneColor = new Color(0, 0, 0, 175);
 
     Level level;
     Boolean pause = false;
 
     JLabel levelLabel;
-
-    private static final int ALPHA = 175;
-    private static final Color GP_BG = new Color(0, 0, 0, ALPHA);
-    private PauseScreen pauseScreen = new PauseScreen();
 
     public GameScreen() {
         this.setLayout(new BorderLayout());
@@ -78,16 +77,17 @@ public class GameScreen extends JPanel implements KeyListener {
         int c = e.getKeyCode();
 
         if (c == KeyEvent.VK_P) {
-            pause = !pause;
-
-            if (pause) {
-                // set the enemies to stop moving
-                // set gravity to zero
-                this.pausePressed();
-            } else {
-                // tell the enemies to start moving again
-                // reset gravity
-            }
+//            pause = !pause;
+//
+//            if (pause) {
+//                // set the enemies to stop moving
+//                // set gravity to zero
+//                this.pausePressed();
+//            } else {
+//                // tell the enemies to start moving again
+//                // reset gravity
+//            }
+            this.pausePressed();
 
             return; // prevents player movement from registering
         }
@@ -124,36 +124,34 @@ public class GameScreen extends JPanel implements KeyListener {
         gameController = controller;
     }
 
+    /**
+     * Creates pause dialog
+     */
     public void pausePressed() {
-        // create our glass pane
         JPanel glassPane = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
-                // magic to make it dark without side-effects
                 g.setColor(getBackground());
                 g.fillRect(0, 0, getWidth(), getHeight());
                 super.paintComponent(g);
             }
         };
-        // more magic below
         glassPane.setOpaque(false);
-        glassPane.setBackground(GP_BG);
+        glassPane.setBackground(glassPaneColor);
 
-        // get the rootpane container, here the JFrame, that holds the JButton
-        RootPaneContainer win = (RootPaneContainer) SwingUtilities.getWindowAncestor(this);
-        win.setGlassPane(glassPane);  // set the glass pane
-        glassPane.setVisible(true);  // and show the glass pane
+        // sets glass pane to give dimmed effect
+        RootPaneContainer window = (RootPaneContainer) SwingUtilities.getWindowAncestor(this);
+        window.setGlassPane(glassPane);
+        glassPane.setVisible(true);
 
-        // create a *modal* JDialog
-        JDialog dialog = new JDialog((Window)win, "", Dialog.ModalityType.APPLICATION_MODAL);
-        dialog.getContentPane().add(pauseScreen);  // add its JPanel to it
-        dialog.setUndecorated(true); // give it no borders (if desired)
-        dialog.pack(); // size it
-        dialog.setLocationRelativeTo((Window) win); // ** Center it over the JFrame **
-        dialog.setVisible(true);  // display it, pausing the GUI below it
+        // create modal JDialog for pause screen
+        JDialog dialog = new JDialog((Window) window, "", Dialog.ModalityType.APPLICATION_MODAL);
+        dialog.getContentPane().add(pauseScreen);
+        dialog.setUndecorated(true);
+        dialog.pack();
+        dialog.setLocationRelativeTo((Window) window);
+        dialog.setVisible(true);
 
-        // at this point the dialog is no longer visible, so get rid of glass pane
         glassPane.setVisible(false);
-
     }
 }
--- src/View/PauseScreen.java
+++ src/View/PauseScreen.java
@@ -5,28 +5,28 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 
 public class PauseScreen extends JPanel {
-    private static final Color BG = new Color(123, 63, 0);
+    private static final Color backgroundColor = new Color(123, 63, 0);
 
     public PauseScreen() {
         JLabel pausedLabel = new JLabel("PAUSED");
         pausedLabel.setForeground(Color.ORANGE);
+        int borderSize = 15;
 
         JPanel pausedPanel = new JPanel();
         pausedPanel.setOpaque(false);
         pausedPanel.add(pausedLabel);
 
-        setBackground(BG);
-        int eb = 15;
-        this.setBorder(BorderFactory.createEmptyBorder(eb, eb, eb, eb));
+        this.setBackground(backgroundColor);
+        this.setBorder(BorderFactory.createEmptyBorder(borderSize, borderSize, borderSize, borderSize));
         this.setLayout(new GridLayout(0, 1, 10, 10));
+
         this.add(pausedPanel);
         this.add(new JButton(new FooAction("RESUME")));
         this.add(new JButton(new FooAction("RESTART")));
         this.add(new JButton(new FooAction("EXIT TO MAP")));
-
     }
 
-    // simple action -- all it does is to make the dialog no longer visible
+    // removes dialog
     private class FooAction extends AbstractAction {
         public FooAction(String name) {
             super(name);
@@ -36,7 +36,7 @@ public class PauseScreen extends JPanel {
         public void actionPerformed(ActionEvent e) {
             Component comp = (Component) e.getSource();
             Window win = SwingUtilities.getWindowAncestor(comp);
-            win.dispose();  // here -- dispose of the JDialog
+            win.dispose();
         }
     }
 }
