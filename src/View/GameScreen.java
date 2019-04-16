1e30528e57fb50b5f2da8e319a7233a1c09de130
Wendy <wryao64@gmail.com>
2019-04-16 20:31:33 +1200

    pause dialog appears when pressed

--- src/View/GameScreen.java
+++ src/View/GameScreen.java
@@ -18,6 +18,10 @@ public class GameScreen extends JPanel implements KeyListener {
 
     JLabel levelLabel;
 
+    private static final int ALPHA = 175;
+    private static final Color GP_BG = new Color(0, 0, 0, ALPHA);
+    private PauseScreen pauseScreen = new PauseScreen();
+
     public GameScreen() {
         this.setLayout(new BorderLayout());
         this.setPreferredSize(new Dimension(WIDTH,HEIGHT));
@@ -74,14 +78,17 @@ public class GameScreen extends JPanel implements KeyListener {
         int c = e.getKeyCode();
 
         if (c == KeyEvent.VK_P) {
+            pause = !pause;
+
             if (pause) {
                 // set the enemies to stop moving
                 // set gravity to zero
+                this.pausePressed();
             } else {
                 // tell the enemies to start moving again
                 // reset gravity
             }
-            pause = !pause;
+
             return; // prevents player movement from registering
         }
 
@@ -115,5 +122,38 @@ public class GameScreen extends JPanel implements KeyListener {
 
     public void setGameController(GameController controller){
         gameController = controller;
+    }
+
+    public void pausePressed() {
+        // create our glass pane
+        JPanel glassPane = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                // magic to make it dark without side-effects
+                g.setColor(getBackground());
+                g.fillRect(0, 0, getWidth(), getHeight());
+                super.paintComponent(g);
+            }
+        };
+        // more magic below
+        glassPane.setOpaque(false);
+        glassPane.setBackground(GP_BG);
+
+        // get the rootpane container, here the JFrame, that holds the JButton
+        RootPaneContainer win = (RootPaneContainer) SwingUtilities.getWindowAncestor(this);
+        win.setGlassPane(glassPane);  // set the glass pane
+        glassPane.setVisible(true);  // and show the glass pane
+
+        // create a *modal* JDialog
+        JDialog dialog = new JDialog((Window)win, "", Dialog.ModalityType.APPLICATION_MODAL);
+        dialog.getContentPane().add(pauseScreen);  // add its JPanel to it
+        dialog.setUndecorated(true); // give it no borders (if desired)
+        dialog.pack(); // size it
+        dialog.setLocationRelativeTo((Window) win); // ** Center it over the JFrame **
+        dialog.setVisible(true);  // display it, pausing the GUI below it
+
+        // at this point the dialog is no longer visible, so get rid of glass pane
+        glassPane.setVisible(false);
+
     }
 }
--- src/View/PauseScreen.java
+++ src/View/PauseScreen.java
@@ -1,6 +1,42 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 
-public class PauseScreen extends JFrame {
+public class PauseScreen extends JPanel {
+    private static final Color BG = new Color(123, 63, 0);
+
+    public PauseScreen() {
+        JLabel pausedLabel = new JLabel("PAUSED");
+        pausedLabel.setForeground(Color.ORANGE);
+
+        JPanel pausedPanel = new JPanel();
+        pausedPanel.setOpaque(false);
+        pausedPanel.add(pausedLabel);
+
+        setBackground(BG);
+        int eb = 15;
+        this.setBorder(BorderFactory.createEmptyBorder(eb, eb, eb, eb));
+        this.setLayout(new GridLayout(0, 1, 10, 10));
+        this.add(pausedPanel);
+        this.add(new JButton(new FooAction("RESUME")));
+        this.add(new JButton(new FooAction("RESTART")));
+        this.add(new JButton(new FooAction("EXIT TO MAP")));
+
+    }
+
+    // simple action -- all it does is to make the dialog no longer visible
+    private class FooAction extends AbstractAction {
+        public FooAction(String name) {
+            super(name);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            Component comp = (Component) e.getSource();
+            Window win = SwingUtilities.getWindowAncestor(comp);
+            win.dispose();  // here -- dispose of the JDialog
+        }
+    }
 }
