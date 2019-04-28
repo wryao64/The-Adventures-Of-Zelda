aab9422e6046f65b95e9fe9daa1802ddce3f99c7
Wendy <wryao64@gmail.com>
2019-04-28 17:42:06 +1200

    tidied up Object code

--- src/Object/Platform.java
+++ src/Object/Platform.java
@@ -3,7 +3,6 @@ package Object;
 import Controller.GameState;
 
 import java.awt.*;
-import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 
 public class Platform extends Object {
@@ -13,50 +12,47 @@ public class Platform extends Object {
 
     Boolean corner;
 
-    public Platform(double x, double y,GameState gameState, Boolean corner){
-        super(50,50,x,y);
-        setImage(gameState,corner);
-        platformImage = loadImage();
+    public Platform(double x, double y, GameState gameState, Boolean corner) {
+        super(50, 50, x, y);
 
+        this.setImage(gameState, corner);
+        platformImage = loadImage();
     }
 
-    private void setImage(GameState level,Boolean corner) {
-        switch(level) {
+    private void setImage(GameState level, Boolean corner) {
+        switch (level) {
             case TUTORIAL:
-                if(corner) {
+                if (corner) {
                     imageLocation = "Assets/tutorialCorner.png";
-                }else{
+                } else {
                     imageLocation = "Assets/tutorialTile.png";
                 }
                 break;
             case LEVEL_1:
-                if(corner) {
+                if (corner) {
                     imageLocation = "Assets/level1Corner.png";
-                }else{
+                } else {
                     imageLocation = "Assets/level1Tile.png";
                 }
                 break;
             case LEVEL_2:
-                if(corner) {
+                if (corner) {
                     imageLocation = "Assets/level2Corner.png";
-                }else{
+                } else {
                     imageLocation = "Assets/level2Tile.png";
                 }
                 break;
             case LEVEL_BOSS:
-                if(corner) {
+                if (corner) {
                     imageLocation = "Assets/bossCorner.png";
-                }else{
+                } else {
                     imageLocation = "Assets/bossTile.png";
                 }
                 break;
         }
     }
 
-    public void paintObject(Graphics2D g){
-        g.drawImage(platformImage, (int) posX, (int) posY, (int)width,
-                (int)height, null);
+    public void paintObject(Graphics2D g) {
+        g.drawImage(platformImage, (int) posX, (int) posY, (int) width, (int) height, null);
     }
-
-
 }
